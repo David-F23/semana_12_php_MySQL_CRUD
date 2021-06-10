7f2b48d4fcc22608e08625a65b6467a6d67b427e
David-FV <floresdavide0@gmail.com>
2021-06-10 17:41:49 -0600

    Se agrego la paginación

--- CRUD_PHP/conexion.php
+++ CRUD_PHP/conexion.php
@@ -6,7 +6,9 @@
 
     try{
         $connect = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME, DB_USER, DB_PASS, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
-
+        $connect->setAttribute(PDO::ATTR_EMULATE_PREPARES, FALSE);
+        $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $connect->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
     }catch(PDOException $e){
         exit('Error: '. $e->getMessage());
     }
--- CRUD_PHP/index.php
+++ CRUD_PHP/index.php
@@ -1,5 +1,5 @@
 <?php
-    include("conexion.php");
+    include_once "conexion.php";
 ?>
 
 <!DOCTYPE html>
@@ -10,6 +10,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Referencia a Bootstrap core CSS --> 
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet"> 
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
     <!-- Referencia a la librería de JQuery --> 
     <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script> 
     <script type="text/javascript">
@@ -202,8 +203,8 @@
             $buscar = $_POST['buscar'];
     ?>
 
-
-        <div class="col-12 col-md-12">
+        <div class="container">
+        <div class="col-12 col-md-12 jumbotron">
             <div class="table-responsive">
                 <table class="table table-bordered table-striped">
                     <thead class='thead-dark'>
@@ -260,6 +261,7 @@
                 </table>
             </div>
         </div>
+        </div>
     <?php
         }
     ?>
@@ -303,38 +305,110 @@
             </thead>
             <tbody> 
                 <?php 
-                $sql = "SELECT * FROM tbl_personal";  
+                /*$sql = "SELECT * FROM tbl_personal";  
                 $query = $connect -> prepare($sql);  
                 $query->execute();  
-                $results = $query->fetchAll(PDO::FETCH_OBJ);
+                $results = $query->fetchAll(PDO::FETCH_OBJ);*/
 
-                if($query->rowCount() > 0)   {  
-                    foreach($results as $result) {  
+                /*if($query->rowCount() > 0)   {  */
+
+                    // cuantas filas quiere ver por página
+                    $registrosPorPagina = 4;
+
+                    // por defecto estamos en la 1; si se envía otra página tomamos esa
+                    $pagina = 1;
+                    if (isset($_GET["pagina"])) {
+                        # tomamos esa
+                        $pagina = $_GET["pagina"];
+                    }
+
+                    // el límite define el número de productos por página 
+                    $limit = $registrosPorPagina;
+
+                    // el offset saltar x cantidad de registros. Multiplicamos la pagina -1
+                    $offset = ($pagina - 1) * $registrosPorPagina;
+
+                    // contar las fila de la tabla
+                    $sentencia = $connect->query("SELECT count(*) AS conteo FROM tbl_personal");
+                    $conteo = $sentencia->fetchObject()->conteo;
+
+                    // obtener la cantidad de páginas a mostrar
+                    $paginas = ceil($conteo / $registrosPorPagina); //3.33333
+
+                    // para obtener los productos (aplicando limit y offset) - preparamos
+                    $sentencia = $connect->prepare("SELECT * FROM tbl_personal LIMIT ? OFFSET ?");
+                    // ejecutamos
+                    $sentencia->execute([$limit, $offset]);
+                    // obtener la colección de filas del select
+                    $registro = $sentencia->fetchAll(PDO::FETCH_OBJ); // ESTÁ LLENO
+                    // con $productos vamos a mostrar las filas en la tabla
+                    foreach($registro as $registros) {  
                         echo "<tr> 
-                        <td>".$result->nombres."</td> 
-                        <td>".$result->apellidos."</td> 
-                        <td>".$result->profesion."</td> 
-                        <td>".$result->estado."</td> 
-                        <td>".$result->fregis."</td> 
+                        <td>".$registros->nombres."</td> 
+                        <td>".$registros->apellidos."</td> 
+                        <td>".$registros->profesion."</td> 
+                        <td>".$registros->estado."</td> 
+                        <td>".$registros->fregis."</td> 
                         <td> 
                         <form method='POST' actino='".$_SERVER['PHP_SELF']."'> 
-                        <input type='hidden' name='id' value='".$result -> id."'> 
+                        <input type='hidden' name='id' value='".$registros->id."'> 
                         <button name='editar' class='btn btn-info btn-sm'>Editar</button> 
                         </form> 
                         </td> 
 
                         <td> 
                         <form  onsubmit=\"return confirm('¿Realmente desea eliminar el registro?');\" method='POST' action='".$_SERVER['PHP_SELF']."'> 
-                        <input type='hidden' name='id' value='".$result -> id."'> 
+                        <input type='hidden' name='id' value='".$registros->id."'> 
                         <button name='eliminar' class='btn btn-danger btn-sm'>Eliminar</button> 
                         </form> 
                         </td> 
                         </tr>"; 
-                } 
+               /* } */
             } 
             ?> 
             </tbody> 
             </table> 
+
+            <nav>
+                <div class="col-12 col-md-12">
+                <div class="row">
+                    <div class="col-xs-12 col-sm-6">
+
+                        <p>Mostrando <?php echo $registrosPorPagina ?> de <?php echo $conteo ?> registros</p>
+                    </div>
+                    <div class="col-xs-12 col-sm-6">
+                        <p>Página <?php echo $pagina ?> de <?php echo $paginas ?> </p>
+                    </div>
+                </div>  
+                <ul class="pagination">
+                    <!-- Si la página actual es mayor a uno, mostramos el botón para ir una página atrás -->
+                    <?php if ($pagina > 1) { ?>
+                        <li>
+                            <a href="index.php?pagina=<?php echo $pagina - 1 ?>">
+                                <span aria-hidden="true">&laquo;</span>
+                            </a>
+                        </li>
+                    <?php } ?>
+                    <!-- Mostramos enlaces para ir a todas las páginas. Es un simple ciclo for-->
+                    <?php for ($x = 1; $x <= $paginas; $x++) { ?>
+                        <li class="<?php if ($x == $pagina) echo "active" ?>">
+                            <a href="index.php?pagina=<?php echo $x ?>">
+                                <?php echo $x ?></a>
+                        </li>
+                    <?php } ?>
+
+                    <!-- Si la página actual es menor al total de páginas, mostramos un botón para ir una página adelante -->
+                    <?php if ($pagina < $paginas) { ?>
+                        <li>
+                            <a href="index.php?pagina=<?php echo $pagina + 1 ?>">
+                                <span aria-hidden="true">&raquo;</span>
+                            </a>
+                        </li>
+                    <?php } ?>
+                </ul>     
+                </div> 
+            </nav>
+
             </div> 
             </div>   
         </div> 
